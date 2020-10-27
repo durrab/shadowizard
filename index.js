03a3dcb97d8f8c756bded58bd5dc3681bad0426b
Durrab <durrab@gmail.com>
2020-10-27 10:48:52 -0700

    fixes errors

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ function shadowwizard(options){
         options.shadow_type = '0px';
     }
     else {
-        options.shodow_type = '15px'
+        options.shadow_type = '15px'
     }
 
     images.forEach(image => {
-        image.style.bowShadow = `10px 10px ${options.shadow_type} 1px rgba(0,0,0,0.12)`
+        image.style.boxShadow = `10px 10px ${options.shadow_type} 1px rgba(0,0,0,0.12)`
         
         if(options.padding){
             image.style.padding = "1em"
